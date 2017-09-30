dc7926aca01dfa8e2ff9ba0a52d74f681abf658b
Julien Champoux <xxredstonetekxx@gmail.com>
2017-09-29 20:04:18 -0400

    0.3

--- src/com/tek/gej/engine/Handler.java
+++ src/com/tek/gej/engine/Handler.java
@@ -1,42 +1,96 @@
+/*
+ * The Handler class will handle your modules
+ * extending the GameInstance class
+ * @see GameInstance.class
+ * It will also manage the gameloop and framerate
+ * 
+ * @author RedstoneTek
+ * @version 0.3
+ */
+
 package com.tek.gej.engine;
 
 import java.util.ArrayList;
 
 public class Handler {
 	
-	boolean exit = false;
+	//Used to stop the gameloop
+	public boolean exit = false;
 	
+	//Stores every loaded module
 	ArrayList<GameInstance> modules = new ArrayList<GameInstance>();
 	
+	//Used to calculate framerate and timing
 	int fps;
-	double rate;
 	
+	/*
+	 * Handler constructor
+	 * @param fps This is the desired framerate
+	 * that the handle will try to achieve
+	 */
 	public Handler(int fps) {
 		this.fps = fps;
-		this.rate = 1000 / fps;
 	}
 	
+	/*
+	 * Initializes the modules and starts
+	 * the main gameloop, will initialize
+	 * every module and then proceed to
+	 * start ticking and rendering each and
+	 * every one of them
+	 */
 	public void init() {
 		
+		/*
+		 * Initializes every window and then
+		 * proceeds to run the user made
+		 * init()
+		 * @see Example.java
+		 */
 		for(GameInstance instance : modules) {
 			instance.preInit();
 			instance.init();
 		}
 		
+		/*
+		 * Starts the gameloop
+		 */
 		startLoop();
 	}
 	
+	/*
+	 * Gameloop, it will simply tick and render
+	 * every module and regulate it at the desired
+	 * framerate 
+	 * @see Class constructor
+	 */
 	private void startLoop() {
+		/*
+		 * Initializes the timing variables to
+		 * calculate and regulate the framerate
+		 */
 		long lastTime = System.nanoTime();
 		double ns = 1000000000 / fps;
 		double delta = 0;
 		long timer = System.currentTimeMillis();
 		int frames = 0;
+		
+		/*
+		 * Gameloop, will run unless exit is
+		 * turned to true.
+		 */
 		while(!exit) {
+			/*
+			 * Regulating framerate and continuing
+			 * if it doesn't match the desired framerate
+			 */
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
 			while(delta >= 1) {
+				/*
+				 * Ticks and renders every module
+				 */
 				for(GameInstance instance : modules) {
 					instance.tick();
 					
@@ -44,10 +98,18 @@ public class Handler {
 					instance.render();
 					instance.postRender();
 				}
+				/*
+				 * Adds up the frames counted and resets the delta
+				 */
 				frames++;
 				delta--;
 			}
 			
+			/*
+			 * Every second, send the new current framerate
+			 * to the modules. Stored in the currentfps var
+			 * @see Example.java
+			 */
 			if(System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
 				for(GameInstance instance : modules) {
@@ -58,6 +120,10 @@ public class Handler {
 		}
 	}
 	
+	/*
+	 * Adds a module to the gameloop and 
+	 * stores the framerate in.
+	 */
 	public void registerModule(GameInstance instance) {
 		modules.add(instance);
 		instance.fps = fps;
